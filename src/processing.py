f7e7d08593ae240a3fd1a5270d894a083c6533b2
admin <e17380539@gmail.com>
2025-11-08 23:12:25 +0400

    добавлена ветка feature/homework_10_1 c файлом processing.py

--- src/processing.py
+++ src/processing.py
@@ -10,18 +10,6 @@ def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[d
     return filtered
 
 
-if __name__ == "__main__":
-    sample_data = [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ]
-    print(filter_by_state(sample_data))
-
-    print(filter_by_state(sample_data, "CANCELED"))
-
-
 def sort_by_date(transactions: list[dict],reverse: bool = True) -> list[dict]:
     """Функция, которая принимает список словарей и необязательный параметр, задающий порядок
      сортировки (по умолчанию — убывание). Функция должна возвращать новый список,
